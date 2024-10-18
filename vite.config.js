ff48be64f60e0879c2dbedae09fb34e96acedc5b
niuniu <10319648+niuniu121@user.noreply.gitee.com>
2024-10-18 16:26:19 +1100

    base

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/mniu-library/' : '/'
+  //base: process.env.NODE_ENV === 'production' ? '/mniu-library/' : '/'
 })
