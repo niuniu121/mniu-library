19f561de3c9d6ab15cd29d0d4c4231a3685471aa
niuniu <10319648+niuniu121@user.noreply.gitee.com>
2024-10-18 14:50:26 +1100

    Add GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production' ? 'mniu-library' : '/'
 })
